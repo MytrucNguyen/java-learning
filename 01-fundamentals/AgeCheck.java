642802854290e58227ee61da7e5b2c09a9cdc0d0
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2026-07-02 16:51:23 -0700

    feat: add age check exercise

---
+++ 01-fundamentals/AgeCheck.java
@@ -0,0 +1,11 @@
+public class AgeCheck {
+    public static void main(String[] args){
+       int age = 30;
+
+       if (age >= 18){
+        System.out.println("You are old");
+       } else {
+        System.out.println("You are a baby");
+       }
+    }
+}
