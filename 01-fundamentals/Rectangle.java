0f13121bb83b79842821322286a3811065a61d0e
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2026-07-02 17:06:32 -0700

    feat: add rectangle area and perimeter exercise

---
+++ 01-fundamentals/Rectangle.java
@@ -0,0 +1,11 @@
+public class Rectangle {
+    public static void main(String[] args){
+        int width = 10;
+        int height = 26;
+        int area = width * height;
+        int perimeter = (width * 2) + (height * 2);
+
+        System.out.println("Area: " + area);
+        System.out.println("Perimeter: " + perimeter);
+    }
+}
